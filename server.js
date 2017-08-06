53727e2f346ee7c4d58e4a5398ce883342bfc809
coolvinaykumar2929 <30519401+coolvinaykumar2929@users.noreply.github.com>
2017-08-06 21:00:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
-    var articleN=res.params.articleName;
+    var articleN=req.params.articleName;
     res.send(createTemplate(articles[articleN] ));
 });
 
