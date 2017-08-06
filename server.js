f4046c4f486c2d50a5d01fb0b8e539e54856e63d
coolvinaykumar2929 <30519401+coolvinaykumar2929@users.noreply.github.com>
2017-08-06 19:31:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send("Article 1 requested and will be served here.");
+    res.sendFile(__dirname,'ui','articl-one.html');
 });
 
 app.get('/article-two',function(req,res){
