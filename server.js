5751513a81de61f115bc987e6cc6d7c8fc19cb13
coolvinaykumar2929 <30519401+coolvinaykumar2929@users.noreply.github.com>
2017-08-06 20:58:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,9 +78,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req,res){
-    var articleName=res.params.articleName;
-    res.send(createTemplate(articles[articleName] ));
+app.get(`/:articleName`,function(req,res){
+    var articleN=res.params.articleName;
+    res.send(createTemplate(articles[articleN] ));
 });
 
 app.get('/ui/style.css', function (req, res) {
