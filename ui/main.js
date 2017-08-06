60848300366b0c81993287983196e7630d0f33c6
coolvinaykumar2929 <30519401+coolvinaykumar2929@users.noreply.github.com>
2017-08-06 23:33:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById("main-txt");
 element.innerHTML="New Value";
+
+var img=document.getElementById("draga");
+img.onclick=function(){
+    img.style.marginLeft="100px";
+};
