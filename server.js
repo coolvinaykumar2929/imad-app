4c407f19f1e608fd57339b00add6684558f2ad63
coolvinaykumar2929 <30519401+coolvinaykumar2929@users.noreply.github.com>
2017-08-12 13:02:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.listen(port, function () {
 });
 
 var names[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
    var name=req.params.name;
    names.push(name);
    //JSON
